226b303136eacf53960dca0afda69b50363bfae7
lucaaroeiracrv <lucaaroeira@gmail.com>
2025-05-23 13:24:40 -0300

    update

--- horse-racing-bet.c
+++ horse-racing-bet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h> // use _sleep no Windows (ou comente se necessário)
+#include <windows.h> // Sleep
 
 #define NUM_CAVALOS 3
 #define DISTANCIA 30
 
 void mostrarCorrida(int posicoes[]) {
-    system("clear"); // use "cls" no Windows
-
+    system("cls"); // para Windows
     for (int i = 0; i < NUM_CAVALOS; i++) {
         printf("Cavalo %d: ", i + 1);
         for (int j = 0; j < posicoes[i]; j++) {
             printf(" ");
         }
-        printf(">>'\n"); // desenho simples do cavalo
+        printf(">>'\n");
     }
 }
 
@@ -23,7 +22,7 @@ int main() {
     int vencedor = -1;
     int cavalo_escolhido;
 
-    printf("Escolha um cavalo de 1 a 3:");
+    printf("Escolha um cavalo de 1 a 3: ");
     scanf("%d", &cavalo_escolhido);
 
     srand(time(0));
@@ -31,22 +30,23 @@ int main() {
     while (vencedor == -1) {
         for (int i = 0; i < NUM_CAVALOS; i++) {
             if (posicoes[i] < DISTANCIA) {
-                posicoes[i] += rand() % 3; // avanço aleatório (0 a 2)
+                posicoes[i] += rand() % 3;
                 if (posicoes[i] >= DISTANCIA && vencedor == -1) {
                     vencedor = i + 1;
                 }
             }
         }
-
         mostrarCorrida(posicoes);
-        usleep(200000); // 200ms
+        Sleep(200); // 200ms
     }
 
     printf("\nO cavalo %d venceu a corrida!\n", vencedor);
-    if(cavalo_escolhido == vencedor) {
-        printf("Parabéns! Você ganhou!");
-    }else{
-        printf("Hahahah você perdeu, passa o dinheiro");
+    if (cavalo_escolhido == vencedor) {
+        printf("Parabéns! Você ganhou!\n");
+    } else {
+        printf("Hahahah você perdeu, passa o dinheiro\n");
     }
+
     return 0;
 }
+// Compilar com: gcc horse-racing-bet.c -o horse-racing-bet.exe
